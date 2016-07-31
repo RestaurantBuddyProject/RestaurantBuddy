a289703778ea955803020d63d66b6598455dd68d
Joey McCoy <joey_mccoy@live.com>
2016-07-30 21:45:59 -0400

    Added new Admin page with restauratn layout

--- RestaurantBuddy/App_Start/BundleConfig.cs
+++ RestaurantBuddy/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace RestaurantBuddy
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.12.4.js",
+                        "~/Scripts/jquery-ui-1.12.0.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- RestaurantBuddy/Controllers/HomeController.cs
+++ RestaurantBuddy/Controllers/HomeController.cs
@@ -13,10 +13,8 @@ namespace RestaurantBuddy.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
+        public ActionResult Admin()
+        {          
             return View();
         }
 
