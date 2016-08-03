4ff065af9483fcba5359c0f92a4cd562e660739b
Joey McCoy <joey_mccoy@live.com>
2016-08-02 22:04:08 -0400

    Added drop functionality

---
+++ RestaurantBuddy/Models/CustomerModel.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace RestaurantBuddy.Models
+//{
+//    public class CustomerModel
+//    {
+//        public CustomerModel(string name, string partySize, string tableType, string time)
+//        {
+//            this.name = name;
+//            this.partySize = partySize;
+//            this.tableType = tableType;
+//            this.time = time;
+//        }
+//        public string name { get; set; }
+//        public string partySize { get; set; }
+//        public string tableType { get; set; }
+//        public string time { get; set; }
+
+
+//    }
+//    public class GetData() {
+//    }
+
+        
+    
+//}
